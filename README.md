ff7199f368083b3b5f792eb4d5a12d497e851ab9
justmogen <justmogen@gmail.com>
2023-12-18 20:45:19 +0300

    Write HTTP server code to handle GET and POST requests, serving files from specified directory

---
+++ README.md
@@ -0,0 +1,50 @@
+# HTTP Server
+
+This is a simple HTTP server written in Python that can handle GET and POST requests. The server can serve files from a specified directory and can also echo back messages sent in the request.
+
+## Requirements
+
+- Python 3.x
+
+## Usage
+
+To run the server, execute the following command:
+
+```bash
+python http_server.py --directory <directory_path>
+Replace <directory_path> with the path to the directory from which files should be served. If no directory is specified, the server will serve files from the current directory.
+
+Once the server is running, you can send HTTP requests to it using curl or any other HTTP client. Here are some examples:
+
+Sending a GET request to the root path /
+
+```bash
+curl -v http://localhost:4221/
+
+This should return a 200 OK response.
+
+Sending a GET request to the /echo path with a message
+
+```bash
+curl -v http://localhost:4221/echo/Hello%20World
+
+This should return a 200 OK response with the message "Hello World."
+
+Sending a GET request to the /user-agent path
+
+```bash
+curl -v -H "User-Agent: My User Agent" http://localhost:4221/user-agent
+
+This should return a 200 OK response with the user agent string.
+
+Sending a GET request to the /files path with a filename
+```bash
+curl -v http://localhost:4221/files/myfile.txt
+
+This should return a 200 OK response with the contents of the file "myfile.txt" in the server's directory.
+
+Sending a POST request to the /files path with a filename and contents
+```bash
+curl -v -X POST -d "Hello World" http://localhost:4221/files/myfile.txt
+
+This should return a 201 Created response and create a file "myfile.txt" in the server's directory with the contents "Hello World."
---
+++ http_server.py
@@ -0,0 +1,105 @@
+import socket
+import threading
+import argparse
+from os import listdir
+from os.path import isfile, join
+
+
+class HttpServer:
+    def __init__(self, directory):
+        self.directory = directory if directory else ""
+
+    def thread_handling(self):
+        server_socket = socket.create_server(("localhost", 4221), reuse_port=True) 
+
+        try:
+            while True:
+                conn, addr = server_socket.accept() # wait client connection
+
+                thread = threading.Thread(target=self.handle_request, args=(conn,))
+                thread.daemon = True
+                thread.start()
+        finally:
+            conn.close()
+
+    def send_response(self, conn, status, response, content_type="text/plain"):
+        conn.sendall(f"HTTP/1.1 {status}\r\n".encode())
+        conn.sendall(f"Content-Type: {content_type}\r\n".encode())
+        conn.sendall(f"Content-Length: {len(response)}\r\n\r\n".encode())
+        conn.sendall(f"{response}\r\n\r\n".encode())
+
+    
+    def handle_request(self, conn):
+        data = conn.recv(1024).decode()
+
+        request = data.split("\r\n")
+        first_line = request[0]
+
+        http_method, path, http_version = first_line.split(" ")
+    
+        headers = {}
+        body = None
+        for i in range(1, len(request)):
+            line = request[i]
+            if line == "":
+                continue
+            elif ": " in line:
+                header, value = line.split(": ")
+                headers[header] = value
+            else:
+                body = line
+
+        if http_method == "GET" and path == "/":
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+        elif http_method == "GET" and path.startswith("/echo"):
+            response = path.removeprefix("/echo/")
+            self.send_response(conn, "200 OK", response)
+
+        elif http_method == "GET" and path == ("/user-agent"):
+            response = headers["User-Agent"]
+            self.send_response(conn, "200 OK", response)
+
+        elif http_method == "GET" and path.startswith("/files"):
+            filename = path.removeprefix("/files/")
+            files = [
+                fl for fl in listdir(self.directory) if isfile(join(self.directory, fl))
+                ]
+            if filename in files:
+                file_content = ""
+                with open(join(self.directory, filename), "r") as content:
+                    file_content = content.read()
+                self.send_response(
+                    conn, "200 OK", file_content, "application/octet-stream"
+                )
+
+            else:
+                self.send_response(conn, "404 Not Found", "")
+
+        elif http_method == "POST" and path.startswith("/files"):
+            filename = path.removeprefix("/files/")
+            try:
+                with open(join(self.directory, filename), "wb") as file:
+                    file.write(body.encode())
+                self.send_response(conn, "201 Created", "")
+            except Exception as e:
+                self.send_response(
+                    conn, "500 Internal Server Error", f"Error writing file: {e}"
+                    )
+        else:
+            self.send_response(conn, "404 Not Found", "")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--directory")
+    args = parser.parse_args()
+
+    directory = args.directory
+
+    server = HttpServer(directory)
+    server.thread_handling()
+
+
+if __name__ == "__main__":
+    main()
+
